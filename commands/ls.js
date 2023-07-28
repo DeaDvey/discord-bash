5c6a4596dfe0fbca01ff63f646b6a306d81f0f86
DeaDvey <maxcrawshaw3@hotmail.com>
2023-07-28 02:33:06 +0100

    added more examples for the ls command

--- commands/ls.js
+++ commands/ls.js
@@ -7,7 +7,7 @@ module.exports = {
 
     async execute(bot, interaction) {
     
-        await interaction.reply({ content: 'List files and directories in the current directory\nDocuments   Music   Pictures   Downloads   Videos', ephemeral: false })
+        await interaction.reply({ content: 'List files and directories in the current directory\n```> ls\nDocuments   Music   Pictures   Downloads   Videos   passwords.txt   cool-selfie.png``````> ls ./Pictures\nCamera Roll   Wallpapers   jotorokujo.png   basil-faulty.jpg   gimp-pfp-design.xcf```', ephemeral: false })
 
     }
 }
