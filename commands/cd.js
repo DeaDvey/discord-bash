b000eeb9b1082adbbb6141024e306526a99839a3
DeaDvey <maxcrawshaw3@hotmail.com>
2023-07-31 23:31:44 +0100

    updated cd command :O

--- commands/cd.js
+++ commands/cd.js
@@ -3,11 +3,35 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('cd')
-    .setDescription('explains cd command'),
-
+    .setDescription('runs the cd command')
+    .addStringOption(option => option
+	.setName('flag1')
+	.setDescription('first flag after base command')
+    ),
     async execute(bot, interaction) {
-    
-        await interaction.reply({ content: 'Change the current directory\n```> cd Pictures\n[Changes directory to Pictures``````> cd ..\n[Changes directory to parent directory]```', ephemeral: false })
+    	const flag1 = interaction.options.getString('flag1')
+
+	if (flag1 === '..') {
+		await interaction.reply({ content: '```[user@discord.com /home] $ ```', ephemeral: false })
+	}
+	else if (flag1 === 'Documents') {
+		await interaction.reply({ content: '```[user@discord.com ~/Documents] $ ```', ephemeral: false })
+	}
+	else if (flag1 === 'Music') {
+                await interaction.reply({ content: '```[user@discord.com ~/Music] $ ```', ephemeral: false })   
+        }
+	else if (flag1 === 'Pictures') {    
+                await interaction.reply({ content: '```[user@discord.com ~/Pictures] $ ```', ephemeral: false })       
+        }
+	else if (flag1 === 'Downloads') {
+                await interaction.reply({ content: '```[user@discord.com ~/Downloads] $ ```', ephemeral: false })    
+        }
+	else if (flag1 === 'Videos') {
+                await interaction.reply({ content: '```[user@discord.com ~/Videos] $ ```', ephemeral: false })    
+        }
+	else {
+		await interaction.reply({ content: '```[user@discord.com ~] $ ```', ephemeral: false })
+	}
 
     }
 }
